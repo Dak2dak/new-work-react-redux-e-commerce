ffec85b2783659a93882c4e91f3e618a62e06c2b
Didier Dakoury <dazibao76@gmail.com>
2022-03-27 17:23:12 +0000

    changes on layoutComponent

--- src/components/layout/LayoutComponent.jsx
+++ src/components/layout/LayoutComponent.jsx
@@ -8,15 +8,17 @@ const LayoutComponent = () => {
     return (
         <div>
             <div className="container-fluid top-layout">
-                <div className="leftside">
-                    <Aside />
-                </div>
-                <div className="middleside">
-                    <CarouselComponent />
-                </div>
-                <div className="rightside">
-                    <div className="rightside__box top-box"></div>
-                    <div className="rightside__box bottom-box"></div>
+                <div className="container layout-container">
+                    <div className="leftside">
+                        <Aside />
+                    </div>
+                    <div className="middleside">
+                        <CarouselComponent />
+                    </div>
+                    <div className="rightside">
+                        <div className="rightside__box top-box"></div>
+                        <div className="rightside__box bottom-box"></div>
+                    </div>
                 </div>
             </div>
         </div>
