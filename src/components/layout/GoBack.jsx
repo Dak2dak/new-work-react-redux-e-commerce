a884dc51f952f38255999a7031f538904c5428dd
Didier Dakoury <dazibao76@gmail.com>
2022-03-31 13:46:53 +0000

    GoBack button features added

--- src/components/layout/GoBack.jsx
+++ src/components/layout/GoBack.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import './GoBack.css';
+import { useEffect, useState } from 'react';
 
 const GoBack = () => {
 
+    const [scrollUpBtn, setScrollUpBtn] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(window.scrollY > 100) {
+                setScrollUpBtn(true)
+            }
+            else {
+                setScrollUpBtn(false)
+            }
+        });
+    }, [])
+
+    const scrollUp = () => {
+        window.scrollTo ({
+            top: 0,
+            behavior: "smooth",
+        })
+    }
+    
+
     return (
         <div>
-            <div className="fixed__button">
-                <button className="floating">
-                    <i className="fa-solid fa-arrow-up"></i>
-                </button>
-            </div>
+            {scrollUpBtn && (
+                <div 
+                    className="fixed__button"
+                    onClick={scrollUp}
+                >
+                    <button className="floating">
+                        <i className="fa-solid fa-arrow-up"></i>
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
